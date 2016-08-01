bccfb0a700a3e4d6d14c17fa7daa2e7bdca439ce
Dylan Albrecht <dylan.alb@gmail.com>
2016-08-01 13:38:52 -0600

    Added some classification analysis and images

--- src/HAR_analysis.py
+++ src/HAR_analysis.py
@@ -4,25 +4,6 @@ Created on Sat Jul 16 14:48:24 2016
 
 @author: dylan
 
-LICENSE:
-
-    This is the HAR_analysis.py file from my data analysis project
-    'di_har_proj'.
-
-    Copyright (C) 2016  Dylan Albrecht
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Description:
-
 This script analyzes the "Human Activity Recognition Using Smartphones Data Set"
 data set (see below for link to the data set).
 
@@ -49,8 +30,30 @@ Output:
         cfm.jpg         -- Normalized confusion matrix for the classification
         db.jpg          -- Decision boundary visualization from the best two
                            features.
+        Laying_db.jpg   -- Decision boundary of classification of individuals
+                           'laying', based on t-SNE projection.
+        Sitting_db.jpg  -- Decision boundary of classification of individuals
+                           'sitting', based on t-SNE projection.
+        Standing_db.jpg -- Decision boundary of classification of individuals
+                           'standing', based on t-SNE projection.
+        Walking_db.jpg  -- Decision boundary of classification of individuals
+                           'walking', based on t-SNE projection.
+        Walking Down Stairs_db.jpg  -- Decision boundary of classification of
+                                       individuals 'walking down stairs', based
+                                       on t-SNE projection.
+        Walking Up Stairs_db.jpg    -- Decision boundary of classification of
+                                       individuals 'walking down stairs', based
+                                       on t-SNE projection.
 
 data source: https://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+Citation:
+
+Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge
+L.Reyes-Ortiz. A Public Domain Dataset for Human Activity Recognition Using
+Smartphones. 21th European Symposium on Artificial Neural Networks,
+Computational Intelligence and Machine Learning, ESANN 2013. Bruges, Belgium
+24-26 April 2013.
 """
 
 import pandas as pd
@@ -58,11 +61,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.colors as colors
+import matplotlib.markers as markers
 
 from sklearn import cross_validation
+from sklearn import manifold
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
+from sklearn.decomposition import TruncatedSVD
+from sklearn.cluster import KMeans
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 from sklearn import metrics
+
+
+#########################
+# Some useful functions
+#########################
 
 def plot_cfm(cm, title='Confusion matrix', cmap=plt.cm.Blues):
     label_list = list(range(31))
@@ -77,9 +91,57 @@ def plot_cfm(cm, title='Confusion matrix', cmap=plt.cm.Blues):
     plt.xlabel('Predicted label')
     
 
+def plot_decision_bdy(X_train_plot, X_test_plot, S_train, S_test, classifier, title='Decision Boundary',
+                      xlabel='Feature 1', ylabel='Feature 2'):
+    # Set up markers for plotting all 30 subjects -- removing 'nothing' markers
+    #ms = list(markers.MarkerStyle.markers)
+    #ms.remove('')
+    #ms.remove(' ')
+    #ms.remove('None')
+    #ms.remove(None)
+
+    plt.figure(figsize=(10,10))
+    subject_cs = cm.Spectral(np.linspace(0, 1, len(np.unique(S_train))))
+
+    # Plot the decision boundaries based on the two feature TRAINING SET
+    x_min, x_max = X_train_plot[:, 0].min() - 1, X_train_plot[:, 0].max() + 1
+    y_min, y_max = X_train_plot[:, 1].min() - 1, X_train_plot[:, 1].max() + 1
+
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, (x_max - x_min)/ 100), np.arange(y_min, y_max, (x_max - x_min)/ 100))
+    Z = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.contourf(xx, yy, Z, cmap=cm.Spectral)
+
+    color_subjects = []
+    for i, c in zip(range(0,len(np.unique(S_train))), list(map(colors.rgb2hex, subject_cs))):
+        idx = np.where(S_test == i)
+        # Scatter plot the TEST DATA
+        #color_subject = plt.scatter(X_test_plot[idx, 0], X_test_plot[idx, 1], c=c, marker=ms[i], s=100, label=i, cmap=cm.Spectral)
+        color_subject = plt.scatter(X_test_plot[idx, 0], X_test_plot[idx, 1], c=c, s=100, label=i)
+        color_subjects.append(color_subject)
+
+    plt.legend(handles=color_subjects)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    
+def get_activity_dataset(data, d_columns, labels, l_columns, activity):
+    X_activity = data[data.activity == activity][d_columns].as_matrix()
+    S_activity = labels[labels.activity == activity][l_columns].as_matrix()
+    
+    return X_activity, S_activity
+
+#######################
+# Script -- Load Data
+#######################
+
 columns = list(map(str, list(range(561))))
 target_name = ['target class']
 subject_name = ['subject name']
+
+activities = {1: 'Walking', 2: 'Walking Up Stairs', 3: 'Walking Down Stairs',
+              4: 'Sitting', 5: 'Standing', 6: 'Laying'}
 
 # Load data and select the columns we're interested in.
 har_train_data = pd.read_fwf('train/X_train.txt', sep=' ', names=columns)
@@ -99,9 +161,22 @@ har_data = har_train_data.append(har_test_data)
 har_activity = har_train_activity.append(har_test_activity)
 har_subject = har_train_subject.append(har_test_subject)
 
+# Full dataset in matrix form
 X = har_data.as_matrix()
+# Full labels (activity label)
 Y = har_activity.as_matrix().ravel()
+# Full labels (user ID)
 S = har_subject.as_matrix().ravel()
+
+#############################
+# For splitting by activity
+
+# Add columns for the labels
+har_data['activity'] = har_activity
+har_data['subject']= har_subject['subject name']
+
+# Add column for activity label
+har_subject['activity'] = har_activity
 
 ################################
 # Training and model selection
@@ -109,13 +184,18 @@ S = har_subject.as_matrix().ravel()
 
 # Perform our own train/test split, at test size 20%
 X_train, X_test, S_train, S_test = cross_validation.train_test_split(X, S, test_size=0.2, random_state=42)
+clf_feature_select = RandomForestClassifier(n_estimators=30, max_depth=None, min_samples_split=1, random_state=0)
+clf_classify = RandomForestClassifier(n_estimators=30, max_depth=None, min_samples_split=1, random_state=0)
 
-clf = RandomForestClassifier(n_estimators=30, max_depth=None, min_samples_split=1, random_state=0)
-
-clf = clf.fit(X_train, S_train)
+# Use a first pass classifier to pick features:
+clf_feature_select = clf_feature_select.fit(X_train, S_train)
 
 # Reduce the features
-model = SelectFromModel(clf, prefit=True)
+model = SelectFromModel(clf_feature_select, prefit=True)
+
+###################################
+# Classify regardless of activity
+
 X_reduced_train = model.transform(X_train)
 X_reduced_test = model.transform(X_test)
 
@@ -123,12 +203,46 @@ print('X_train.shape: ', X_train.shape)
 print('X.shape: ', X_reduced_train.shape)
 
 # Train again based on the restricted set of features
-clf = clf.fit(X_reduced_train, S_train)
-
+clf_classify = clf_classify.fit(X_reduced_train, S_train)
+    
 # Let's see how well the new classifier does (should be avg: 92%)
-print('score', clf.score(X_reduced_test, S_test))
-predicted = clf.predict(X_reduced_test)
+print('score', clf_classify.score(X_reduced_test, S_test))
+predicted = clf_classify.predict(X_reduced_test)
 print(metrics.classification_report(S_test, predicted))
+
+##############################
+# Classify for each activity
+
+# T-SNE -- 2d reduction and clustering.
+tsne = manifold.TSNE(n_components=2, random_state=0)
+reduced_clf = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=1, random_state=0)
+
+for a in list(activities):
+    Xa, Sa = get_activity_dataset(har_data, columns, har_subject, 'subject name', a)
+    
+    # T-SNE and classification of clusters
+    Xa_plot = tsne.fit_transform(Xa)
+    Xa_train_plot, Xa_test_plot, Sa_train, Sa_test = cross_validation.train_test_split(Xa_plot, Sa, test_size=0.2, random_state=42)
+
+    # Train a classifier on the projected data:
+    reduced_clf.fit(Xa_train_plot, Sa_train)
+
+    a_score = reduced_clf.score(Xa_test_plot, Sa_test)
+    #a_predicted = clf_classify.predict(Xa_test_plot)
+    #print(metrics.classification_report(S_test, predicted))
+    print('len(Xa_train_plot): %d; len(Xa_test_plot): %d' % (len(Xa_train_plot), len(Xa_test_plot)))
+
+    # Plot the test data in the training data contours
+    a_title = 'Subject Decision Boundary; ' + activities[a] + '; Score: ' + '%.1f%%' % (a_score * 100)
+    a_xlabel = 'Embedded dimension 1'
+    a_ylabel = 'Embedded dimension 2'
+    plot_decision_bdy(Xa_train_plot, Xa_test_plot, Sa_train, Sa_test,
+                      reduced_clf, a_title, a_xlabel, a_ylabel)
+                      
+    a_fname = activities[a] + '_db.jpg'
+    plt.savefig(a_fname)
+
+    plt.show()
 
 #############
 # Plotting
@@ -141,51 +255,26 @@ cfm_fig = plt.figure(figsize=(10,10))
 plot_cfm(cfm_normalized, title='Normalized Confusion Matrix')
 plt.savefig('cfm.jpg')
 
-# Now for the decision boundary visualization
-
 # Grab the two best features for plotting purposes
-ifeats = list(clf.feature_importances_)
+ifeats = list(clf_classify.feature_importances_)
 i1f = ifeats.index(np.max(ifeats))
 ifeats.remove(np.max(ifeats))
 i2f = ifeats.index(np.max(ifeats))
 
-print('feature_importances[%d,%d]: %s' % (i1f, i2f, clf.feature_importances_[[i1f, i2f]]))
+print('feature_importances[%d,%d]: %s' % (i1f, i2f, clf_classify.feature_importances_[[i1f, i2f]]))
 
 X_2feat_train = X_reduced_train[:,[i1f, i2f]]
 X_2feat_test = X_reduced_test[:, [i1f, i2f]]
 
 # Refit, so we can predict!
-clf.fit(X_2feat_train, S_train)
+clf_classify.fit(X_2feat_train, S_train)
 
 # Let's see how well our (too) much reduced set does (not very good, but
 # reasonable for visualization purposes)
-predicted = clf.predict(X_2feat_test)
+predicted = clf_classify.predict(X_2feat_test)
 print(metrics.classification_report(S_test, predicted))
 
-# Color map for the class labels
-cs = cm.Spectral(np.linspace(0, 1, len(np.unique(S_test))))
-
-# Plot the decision boundaries based on the two feature TRAINING SET
-x_min, x_max = X_2feat_train[:, 0].min() - 1, X_2feat_train[:, 0].max() + 1
-y_min, y_max = X_2feat_train[:, 1].min() - 1, X_2feat_train[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, (x_max - x_min)/ 100), np.arange(y_min, y_max, (x_max - x_min)/ 100))
-Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-
-decision_bdy_fig = plt.figure(figsize=(10,10))
-plt.contourf(xx, yy, Z, cmap=cm.Spectral)
-
-# plot the TEST SET data points
-color_classes = []
-for i, c in zip(range(0,len(np.unique(S_test))), list(map(colors.rgb2hex, cs))):
-    idx = np.where(S_test == i)
-    color_class = plt.scatter(X_2feat_test[idx, 0], X_2feat_test[idx, 1], c=c, label=i, cmap=cm.Spectral)
-    color_classes.append(color_class)
-
-plt.title('Decision Boundary')
-plt.xlabel('Feature #1')
-plt.ylabel('Feature #2')
-plt.legend(handles=color_classes)
+plot_decision_bdy(X_2feat_train, X_2feat_test, S_train, S_test, clf_classify)
 plt.savefig('db.jpg')
 
 plt.show()
